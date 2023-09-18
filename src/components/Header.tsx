469ba6535efce0bde7b8e0133a3481ff7c26d20f
Enes Taha Öz <enestahaoz@gmail.com>
2023-09-18 19:29:25 +0300

    complete header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import styles from '../css_files/header.module.css';
 
 function Header() {
   return (
-    <div>header</div>
-  )
+    <div className={styles.headerContainer}>
+      <p className={styles.headerNameText}>Enes Taha Öz |</p>
+      <div className={styles.headerLinkContainer}>
+        <Link className={styles.headerLink} href={'/'}>
+          HOME
+        </Link>
+        <Link className={styles.headerLink} href={'/about'}>
+          ABOUT
+        </Link>
+        <Link className={styles.headerLink} href={'/projects'}>
+          PROJECTS
+        </Link>
+        <Link className={styles.headerLink} href={'/cv'}>
+          CV
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
