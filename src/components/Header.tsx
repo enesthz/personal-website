522c6a725e6c04cfc763f8b87a6415e54c938712
Enes Taha Öz <enestahaoz@gmail.com>
2023-09-23 01:27:12 +0300

    add responsive design for width <600px

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
+'use client';
+
 import Link from 'next/link';
 import styles from '../css_files/header.module.css';
+import HeaderDropdown from './HeaderDropdown';
+
+const breakpoint600 = window.matchMedia('(max-width:600px)');
 
 function Header() {
   return (
     <div className={styles.headerContainer}>
       <p className={styles.headerNameText}>Enes Taha Öz |</p>
-      <div className={styles.headerLinkContainer}>
-        <Link className={styles.headerLink} href={'/'}>
-          HOME
-        </Link>
-        <Link className={styles.headerLink} href={'/about'}>
-          ABOUT
-        </Link>
-        <Link className={styles.headerLink} href={'/projects'}>
-          PROJECTS
-        </Link>
-        <a className={styles.headerLink} href={'/CV.pdf'} download>
-          CV
-        </a>
-      </div>
+      {breakpoint600.matches ? (
+        <HeaderDropdown dropdownListItems={['home', 'about', 'projects', 'cv']}></HeaderDropdown>
+      ) : (
+        <div className={styles.headerLinkContainer}>
+          <Link className={styles.headerLink} href={'/'}>
+            HOME
+          </Link>
+          <Link className={styles.headerLink} href={'/about'}>
+            ABOUT
+          </Link>
+          <Link className={styles.headerLink} href={'/projects'}>
+            PROJECTS
+          </Link>
+          <a className={styles.headerLink} href={'/CV.pdf'} download>
+            CV
+          </a>
+        </div>
+      )}
     </div>
   );
 }
