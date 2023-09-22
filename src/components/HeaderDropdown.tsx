77efa06e4a6dbab2a4681c52d7296986beb4e0a0
Enes Taha Öz <enestahaoz@gmail.com>
2023-09-22 23:05:23 +0300

    complete Header Dropdown to be used for small devices

---
+++ src/components/HeaderDropdown.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
+import styles from 'src/css_files/headerDropdown.module.css';
+
+function HeaderDropdown({ dropdownListItems }: { dropdownListItems: string[] }) {
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+
+  function handleClickMenuIcon(event: React.MouseEvent<HTMLDivElement>) {
+    event.preventDefault();
+    setIsDropdownOpen(!isDropdownOpen);
+    if (isDropdownOpen) {
+      document.getElementById('dropdownMenu')!.className = '';
+    } else {
+      document.getElementById('dropdownMenu')!.className = styles.headerDropdownMenu;
+    }
+  }
+
+  const dropdownList = dropdownListItems.map((item) => {
+    return item !== 'cv' ? (
+      <Link className={styles.headerDropdownLinks} href={`/${item !== 'home' ? item : ''}`}>
+        {item}
+      </Link>
+    ) : (
+      <a href='/CV.pdf' download>
+        CV
+      </a>
+    );
+  });
+
+  return (
+    <div className={styles.headerDropdown}>
+      <div id='menuIcon' onClick={handleClickMenuIcon}>
+        <AiOutlineMenu color={'white'} size={'1.5rem'} />
+      </div>
+      <div id='dropdownMenu'>{isDropdownOpen && dropdownList}</div>
+    </div>
+  );
+}
+
+export default HeaderDropdown;
